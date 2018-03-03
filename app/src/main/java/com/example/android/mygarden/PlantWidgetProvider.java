c814aa47e016a998d9e715319c74f451276421d6
xjoliez <skusmiawati@gmail.com>
2018-03-03 15:22:29 +0700

    Add New Widget

---
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.example.android.mygarden;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by selina on 3/3/2018.
+ */
+
+public class PlantWidgetProvider extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
